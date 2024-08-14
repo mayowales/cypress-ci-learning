8db59d7c8c2c4fd8cafb820194c729ba71eb4bd8
Wale <walekolade45@gmail.com>
2024-08-14 22:37:54 +0200

    fix multi cucumber reporter

--- multiReporter.js
+++ multiReporter.js
@@ -1,17 +1,6 @@
 const report = require("multiple-cucumber-html-reporter");
 
 report.generate({
-  jsonDir: "./cypress/reports/json/",
-  reportPath: "./cypress/reports/html/cucumber-report/",
-  metadata: {
-    browser: {
-      name: "chrome",
-      version: "60",
-    },
-    device: "Local test machine",
-    platform: {
-      name: "ubuntu",
-      version: "16.04",
-    },
-  },
+  jsonDir: "./cypress/reports/json",
+  reportPath: "./cypress/reports/html/"
 });
